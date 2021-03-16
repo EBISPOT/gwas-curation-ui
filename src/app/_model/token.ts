f44a34bb12d31012650a1a712441388ab4cae186
sprintell <abayomi@sprintell.org>
2021-03-16 18:13:23 +0000

    Add gwas jwt token model

---
+++ src/app/_model/token.ts
@@ -0,0 +1,13 @@
+export class Token {
+    'iss': string;
+    'jti': string;
+    'iat': number;
+    'sub': string;
+    'email': string;
+    'nickname': string;
+    'name': string;
+    'domains': [
+      string
+    ];
+    'exp': number;
+}
