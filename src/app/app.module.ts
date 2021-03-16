867def8d5c30a73f4852ef06f783e6f516e404a1
sprintell <abayomi@sprintell.org>
2021-03-16 17:59:31 +0000

    Add routing to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { authInterceptorProviders } from './_security/auth.interceptor';
+import { AuthGuard } from './_security/auth.guard';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthGuard, authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
