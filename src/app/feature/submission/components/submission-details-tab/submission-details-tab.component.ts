628d88b254c4409f47a606828911f8bb1835bee0
ala <ala@ebi.ac.uk>
2021-04-11 22:34:15 +0100

    v1 details

--- src/app/feature/submission/components/submission-details-tab/submission-details-tab.component.ts
+++ src/app/feature/submission/components/submission-details-tab/submission-details-tab.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Submission } from '../../../../core/models/submission';
 
 @Component({
   selector: 'app-submission-details-tab',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmissionDetailsTabComponent implements OnInit {
 
+  @Input() submission: Submission;
   constructor() { }
 
   ngOnInit(): void {
