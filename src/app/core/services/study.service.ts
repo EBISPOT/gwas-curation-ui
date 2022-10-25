a2dde8aab1540ef95a7bd946019325a4991a8ff4
ala <63405719+ala-ebi@users.noreply.github.com>
2022-10-25 12:03:20 +0100

    remove quotes from params and minor ui improvements

--- src/app/core/services/study.service.ts
+++ src/app/core/services/study.service.ts
@@ -22,13 +22,13 @@ export class StudyService {
       params = params.set('sort', sort + ',' + order);
     }
     if (efoTrait){
-      params = params.set('efoTrait', '"' + efoTrait + '"');
+      params = params.set('efoTrait', efoTrait);
     }
     if (reportedTrait) {
-      params = params.set('reportedTrait', '"' + reportedTrait + '"');
+      params = params.set('reportedTrait', reportedTrait);
     }
     if (note) {
-      params = params.set('note', '"' + note + '"');
+      params = params.set('note', note);
     }
     if (sumstatsFlag !== 'any' && sumstatsFlag != null) {
       params = params.set('sumstatsFlag', String(sumstatsFlag));
